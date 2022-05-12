e00f371244ab4b3880a8a6a1e0cf30da052f913a
diphan-source <ahabwediphan@yahoo.com>
2022-05-12 06:17:33 -0700

    ft-add index.php

---
+++ heroku/inc/Footer.php
@@ -0,0 +1,90 @@
+ <!-- main-footer -->
+ <section class="main-footer">
+            <div class="footer-top">
+                <div class="auto-container">
+                    <div class="row clearfix">
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget about-widget">
+                                <div class="title-box">
+                                    <div class="icon-box"><i class="icon-hand"></i></div>
+                                    <span>Let's better Our Community</span>
+                                    <h3>Partner With Us</h3>
+                                </div>
+                                <div class="text">
+                                    <p>Prevents our being able to do we like best, every pleasures is every pain avoided.</p>
+                                    <a href="index.php" class="theme-btn btn-one">Join With Us</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget links-widget ml-30">
+                                <div class="widget-title">
+                                    <h3>Quick Links</h3>
+                                </div>
+                                <div class="widget-content">
+                                    <ul class="links-list clearfix">
+                                        <li><a href="about.php">About Charity</a></li>
+                                        <li><a href="volunteer.php">Meet Our Volunteers</a></li>
+                                        <li><a href="events.php">Upcoming  Events</a></li>
+                                        <li><a href="blog.php">Blog & Articles</a></li>
+                                        <li><a href="donations.php">Our Causes</a></li>
+                                        <li><a href="contact.php">Get Touch With Us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget links-widget ml-30">
+                                <div class="widget-title">
+                                    <h3>For Donars</h3>
+                                </div>
+                                <div class="widget-content">
+                                    <ul class="links-list clearfix">
+                                        <li><a href="index.php">Crisis Relief Centre</a></li>
+                                        <li><a href="index.php">Round Up to Give</a></li>
+                                        <li><a href="index.php">Refer a Friend</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget contact-widget ml-30">
+                                <div class="title-box">
+                                    <div class="icon-box"><i class="icon-donation-2"></i></div>
+                                    <span>Ntoroko Development Network</span>
+                                    <h3>Giving / Enquiry</h3>
+                                </div>
+                                <div class="widget-content">
+                                    <div class="single-item">
+                                        <h3><a href="tel:23345678901">(+256) 777-000-000</a></h3>
+                                        <p><a href="mailto:example@info.com">example@info.com</a></p>
+                                    </div>
+                                    <div class="single-item">
+                                        <h5>Ntoroko District</h5>
+                                        <p>Western Region , ntoroko Town </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="auto-container">
+                    <div class="inner-box clearfix">
+                        <div class="copyright pull-left">
+                            <p>&copy; 2022 <a href="index.php">Ntoroko Development Network,</a> All Rights Reserved.</p>
+                        </div>
+                        <!-- <ul class="footer-card pull-right clearfix">
+                            <li><span>Ways to Donate:</span></li>
+                            <li><a href="index.php"><img src="assets/images/resource/card-1.png" alt=""></a></li>
+                            <li><a href="index.php"><img src="assets/images/resource/card-2.png" alt=""></a></li>
+                            <li><a href="index.php"><img src="assets/images/resource/card-3.png" alt=""></a></li>
+                            <li><a href="index.php"><img src="assets/images/resource/card-4.png" alt=""></a></li>
+                            <li><a href="index.php"><img src="assets/images/resource/card-5.png" alt=""></a></li>
+                        </ul> -->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- main-footer end -->
